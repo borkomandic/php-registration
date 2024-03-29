95078318766413cc86dd2ec59646dfdb4e642816
Borko Mandic <borkomandic@gmail.com>
2024-03-29 18:48:48 +0100

    routing to index.php added

--- public/index.php
+++ public/index.php
@@ -8,5 +8,23 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
-$controller = new RegistrationController();
-$controller->register();
+// Capture the 'my-app-path' parameter as the routing path
+$myAppPath = $_GET['my-app-path'] ?? '';
+
+// Simple routing mechanism
+switch ($myAppPath) {
+    case 'register':
+        // You can access other query parameters as usual, for example:
+        // $param = $_GET['param'] ?? 'default_value';
+
+        $controller = new RegistrationController();
+        $controller->register();
+        break;
+
+    // More cases for different routes can be added here
+
+    default:
+        echo "404 Not Found";
+        break;
+}
+
